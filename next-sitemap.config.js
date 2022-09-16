f54dc2370110ae64b5c5dc6a1760bb15549303fc
HidemaruOwO <hidemal@outlook.jp>
2022-09-16 21:12:03 +0900

    :sparkles: quick push (next-sitemap.config.js public/robots.txt)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   siteUrl: "https://v-sli.me",
   generateRobotsTxt: true,
   outDir: "./out",
+  siteMapSize: 7000,
+  additionalSitemaps: ["https://v-sli.me/sitemap-0.xml"],
 };
