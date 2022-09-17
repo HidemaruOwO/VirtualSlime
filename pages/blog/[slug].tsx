4b87f275f78dd5e77ef1a56f8cff9f5a636d6634
HidemaruOwO <hidemal@outlook.jp>
2022-09-18 08:46:59 +0900

    :sparkles: Adsenseの配置 (pages/blog/[slug].tsx)

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -111,6 +111,20 @@ export default function PostPage({
               __html: marked(content),
             }}
           ></div>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3960648628437030"
+            crossOrigin="anonymous"
+          ></script>
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-3960648628437030"
+            data-ad-slot="8178524991"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </div>
       </div>
     </Layout>
