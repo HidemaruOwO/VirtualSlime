50c85ad3a21967db8da2a59b0597ac0861199a32
HidemaruOwO <hidemal@outlook.jp>
2022-11-22 22:09:24 +0900

    :wrench: next-sitemapの追加 (cache/data.ts next-sitemap.config.js package.json yarn.lock)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,4 @@ module.exports = {
   siteUrl: "https://v-sli.me",
   generateRobotsTxt: true,
   outDir: "./out",
-  siteMapSize: 7000,
-  additionalSitemaps: ["https://v-sli.me/sitemap-0.xml"],
 };
