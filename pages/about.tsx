2696b394139225d8d34b0963bf16e364e2c56d8f
HidemaruOwO <hidemal@outlook.jp>
2022-12-07 22:57:34 +0900

    :sparkles: quick push (README.md pages/about.tsx)

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 export default function AboutPage() {
   return (
-    <Layout title="About DevSpace">
+    <Layout
+      title="About Virtual Slime"
+      description="v-sli.meのプライバシーポリシーです"
+    >
       <h1 className="text-5xl border-b-4 pb-5 font-bold">About</h1>
 
       <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
