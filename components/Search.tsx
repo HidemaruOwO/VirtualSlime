98d3ac33ad0f7f5c0d4942070d6d8713c1ed7aa2
HidemaruOwO <hidemal@outlook.jp>
2022-12-07 22:22:44 +0900

    :art: 黒帯を短く変更 (components/Search.tsx)

--- components/Search.tsx
+++ components/Search.tsx
@@ -21,9 +21,9 @@ export default function Search() {
   }, [searchTerm]);
 
   return (
-    <div className="relative bg-gray-600 p-4">
+    <div className="relative bg-gray-900 p-4 h-16">
       <div className="container mx-auto flex items-center justify-center md:justify-end">
-        <div className="relative text-gray-600 w-82">
+        <div className="relative text-gray-600 w-82 bottom-3">
           {/*  
 <form>
             <input
