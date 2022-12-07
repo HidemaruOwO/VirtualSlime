248969209d81a4748ba6f8b6d1e30699994a43cd
HidemaruOwO <hidemal@outlook.jp>
2022-12-07 22:07:04 +0900

    :snowman: i18nの無効化 (next.config.js)

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
 const withExportImages = require("next-export-optimize-images");
 
-module.exports = withExportImages({
-  // write your next.js configuration values.
-  // next.config.js
-  i18n: {
-    defaultLocale: "ja",
-  },
-  domains: [
-    {
-      domain: "v-sli.me",
-      defaultLocale: "ja",
-    },
-    {
-      domain: "example.en",
-      defaultLocale: "en",
-    },
-  ],
-});
+module.exports = withExportImages();
