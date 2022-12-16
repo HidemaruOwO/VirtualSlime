d378198b523135c4ea68286e8535729722ce5058
HidemaruOwO <hidemal@outlook.jp>
2022-12-17 00:46:08 +0900

    :art: gridサイズの変更 (pages/blog/[slug].tsx pages/blog/page/[page_index].tsx pages/index.tsx)

--- pages/blog/page/[page_index].tsx
+++ pages/blog/page/[page_index].tsx
@@ -11,10 +11,10 @@ export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
       <div className="flex justify-between flex-col md:flex-row">
-        <div className="w-3/4 mr-10 center">
+        <div className="center">
           <h1 className="text-5xl border-b-4 p-5 font-bold">✏️ Blog</h1>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
             {posts.map((post: any, index: number) => (
               <Post key={index} post={post} />
             ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function HomePage({ posts }) {
       </Head>
       <h1 className="text-5xl border-b-4 p-5 font-bold">📮 Latest Articles</h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {posts.map((post: any, index: number) => (
           <Post key={index} post={post} />
         ))}
