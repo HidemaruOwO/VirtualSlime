76ddbc843652d46ed9cad53524ea9f7eb7512a6c
HidemaruOwO <hidemal@outlook.jp>
2022-12-17 22:47:05 +0900

    :sparkles: metaのcharsetの追加 (components/Layout.tsx)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,7 @@ export default function Layout({ title, keywords, description, children }) {
     <div>
       <Head>
         <title>{title}</title>
+        <meta charSet="utf-8" />
         <meta name="keywords" content={keywords} />
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
