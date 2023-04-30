df168f2c84604fa30644d673864689d68984667b
HidemaruOwO <hidemal@outlook.jp>
2023-04-30 20:16:29 +0900

    :snowman: zenn-markdown-blogに移行 (package.json, pages/_app.tsx, pages/blog/[slug].tsx, posts/genshin-ver35-wish-characters.md, styles/globals.css, yarn.lock)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import "zenn-content-css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("zenn-content-css");
+  }, []);
   return <Component {...pageProps} />;
 }
 
