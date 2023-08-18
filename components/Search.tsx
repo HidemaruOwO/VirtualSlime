19107fea8f718f2abf885a179d670d86563d815a
HidemaruOwO <hidemal@outlook.jp>
2023-08-18 23:54:57 +0900

    :snowman: Searchのfeych先をapi serverに変更 (components/Search.tsx)

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,28 +3,31 @@ import SearchResults from "./SearchResults";
 import React from "react";
 
 export default function Search() {
-   const [searchTerm, setSearchTerm] = useState("");
-   const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-   useEffect(() => {
-      const getResults = async () => {
-         if (searchTerm === "") {
-            setSearchResults([]);
-         } else {
-            const res = await fetch(`/api/search?q=${searchTerm}`);
-            const { results } = await res.json();
-            setSearchResults(results);
-         }
-      };
+  useEffect(() => {
+    const getResults = async () => {
+      if (searchTerm === "") {
+        setSearchResults([]);
+      } else {
+        // const res = await fetch(`/api/search?q=${searchTerm}`);
+        const res = await fetch(
+          `https://api.v-sli.me/api/posts?q=${searchTerm}`
+        );
+        const { results } = await res.json();
+        setSearchResults(results);
+      }
+    };
 
-      getResults();
-   }, [searchTerm]);
+    getResults();
+  }, [searchTerm]);
 
-   return (
-      <div className="relative bg-gray-900 p-4 h-12">
-         <div className="container mx-auto flex items-center justify-center md:justify-end">
-            <div className="relative text-gray-600 w-82 bottom-3">
-               {/*  
+  return (
+    <div className="relative bg-gray-900 p-4 h-12">
+      <div className="container mx-auto flex items-center justify-center md:justify-end">
+        <div className="relative text-gray-600 w-82 bottom-3">
+          {/*  
 <form>
             <input
               type="search"
@@ -39,23 +42,23 @@ export default function Search() {
             <FaSearch className="absolute top-0 right-0 text-black mt-3 mr-4" />
           </form>
 */}
-               <form className="search-container">
-                  <input
-                     id="search-box"
-                     type="text"
-                     className="search-box"
-                     value={searchTerm}
-                     onChange={(e) => setSearchTerm(e.target.value)}
-                     placeholder="Search Articles..."
-                  />
-                  <label htmlFor="search-box">
-                     <i className="zmdi zmdi-search search-icon" />
-                  </label>
-               </form>
-            </div>
-         </div>
-
-         <SearchResults results={searchResults} />
+          <form className="search-container">
+            <input
+              id="search-box"
+              type="text"
+              className="search-box"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Search Articles..."
+            />
+            <label htmlFor="search-box">
+              <i className="zmdi zmdi-search search-icon" />
+            </label>
+          </form>
+        </div>
       </div>
-   );
+
+      <SearchResults results={searchResults} />
+    </div>
+  );
 }
