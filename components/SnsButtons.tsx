fd26f63f44857d1c4780201587cfaa83485fe7d1
HidemaruOwO <hidemal@outlook.jp>
2023-08-19 01:47:51 +0900

    🎨 mi-shareボタンの追加 (components/SnsButtons.tsx, public/images/misskey-logo.svg, styles/SnsButtons.css, styles/icomoon/demo.html, styles/icomoon/fonts/icomoon.eot, styles/icomoon/fonts/icomoon.ttf, styles/icomoon/fonts/icomoon.woff, styles/icomoon/selection.json, styles/icomoon/style.css)

--- components/SnsButtons.tsx
+++ components/SnsButtons.tsx
@@ -14,6 +14,7 @@ export default function SnsButtons({ title }: Props) {
     { name: "twitter", url: "https://twitter.com/intent/tweet?url=" },
     { name: "facebook", url: "https://www.facebook.com/sharer/sharer.php?u=" },
     { name: "line", url: "https://social-plugins.line.me/lineit/share?url=" },
+    { name: "misskey", url: "https://misskeyshare.link/share.html?text=" },
   ];
 
   const router = useRouter();
@@ -29,7 +30,17 @@ export default function SnsButtons({ title }: Props) {
         <i className="zmdi zmdi-facebook"></i>
       </a>
       <a href={sites[2].url + url}>
-        <i className="icon-line"></i>{" "}
+        <i className="icon-line"></i>
+      </a>
+      <a href={sites[3].url + title + "\n" + url}>
+        <div
+          className="relative mx-auto"
+          style={{ width: 66 + "px", height: 66 + "px" }}
+        >
+          <div className="text-center m-auto absolute inset-x-0 inset-y-0 w-12 h-12">
+            <img width={32} height={32} src="/images/misskey-logo.svg" />
+          </div>
+        </div>
       </a>
     </div>
   );
