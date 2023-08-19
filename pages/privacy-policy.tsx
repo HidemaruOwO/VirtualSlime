e2ab21c2c3e1d17fbb9055291b031a0da43594e3
HidemaruOwO <hidemal@outlook.jp>
2023-08-19 16:43:34 +0900

    :snowman: プライバシーポリシーの改定 (pages/privacy-policy.tsx)

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -1,12 +1,13 @@
 import Layout from "../components/Layout";
+import React from "react";
 
 export default function PrivacyPolicyPage() {
   return (
     <Layout
       title="Privacy Policy"
-      description="v-sli.meのプライバシーポリシーです"
+      description="Virtual Slimeのプライバシーポリシー"
     >
-      <h1 className="text-5xl border-b-4 pb-5 font-bold">
+      <h1 className="mx-4 text-5xl border-b-4 pb-5 font-bold">
         プライバシーポリシー
       </h1>
 
@@ -21,10 +22,10 @@ export default function PrivacyPolicyPage() {
           本ポリシーは、本サイトがどのような個人情報を取得し、どのように利用・共有するか、ユーザーがどのようにご自身の個人情報を管理できるかをご説明するものです。
           <br />
           <h3 className="text-2xl my-2 font-medium text-slate-700">
-            1.事業者情報
+            1.管理者情報
           </h3>
           本サイトはひでまる個人が運営するサイトです。 Email: support@hide0.net
-          Discord: ひでまる#0743
+          Discord: Hidemaru_OwO
           <h3 className="text-2xl my-2 font-medium text-slate-700">
             2.個人情報の取得方法
           </h3>
@@ -42,7 +43,7 @@ export default function PrivacyPolicyPage() {
             5.保有個人データの開示、訂正
           </h3>
           本サイトは本人から個人情報の開示を求められたときには、遅滞なく本人に対しこれを開示します。個人情報の利用目的の通知や訂正、追加、削除、利用の停止を希望される方は以下の手順でご請求ください。
-          Email: support@hide0.net Discord: ひでまる#0743
+          Email: support@hide0.net Discord: Hidemaru_OwO
           <h3 className="text-2xl my-2 font-medium text-slate-700">
             6.個人情報取り扱いに関する相談や苦情の連絡先
           </h3>
@@ -61,8 +62,10 @@ export default function PrivacyPolicyPage() {
           本サイトがcookieとして送るファイルは、個人を特定するような情報は含んでおりません。
           お使いのWebブラウザの設定により、cookieを無効にすることも可能です。
           <h3 className="text-2xl my-2 font-medium text-slate-700">
-            9.プライバシーポリシーの制定日及び改定日 制定: 2022年12月07日
+            9.プライバシーポリシーの制定日及び改定日
           </h3>
+          制定: 2022年12月07日 <br />
+          改定: 2023年8月19日
         </p>
       </div>
     </Layout>
