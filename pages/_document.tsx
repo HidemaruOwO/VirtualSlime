2c8b340158d7d521a6f862dfb1ef79939f6b3892
HidemaruOwO <hidemal@outlook.jp>
2023-09-04 07:48:53 +0900

    🔧 lang=ja似設定 (pages/_document.tsx)

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
