5638c338fd140644c963b71a956c6b6699fb2147
HidemaruOwO <hidemal@outlook.jp>
2023-10-26 11:37:09 +0900

    ✨ Add react (astro.config.mjs, bun.lockb, package.json, tsconfig.json, yarn.lock)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), react()]
 });
