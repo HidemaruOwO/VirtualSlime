61760ac23264723b52fdfe46f10f2a1fb06a4bfe
HidemaruOwO <hidemal@outlook.jp>
2023-12-01 11:18:59 +0900

    🔧 RSSにcontentを含ませる (src/pages/rss.xml.js)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,30 @@
 import rss from "@astrojs/rss";
 import path from "path";
 import { convDateToHyphen } from "../lib/date";
+import { mdToHtml } from "../lib/markdown";
 
-export const get = () => {
+export const get = async () => {
   const postImportResult = import.meta.glob("../../posts/*.md", {
     eager: true,
   });
   const posts = Object.values(postImportResult);
+
+  const items = await Promise.all(
+    posts.map(async (post) => ({
+      title: post.frontmatter.title,
+      pubDate: convDateToHyphen(post.frontmatter.date),
+      description: post.frontmatter.excerpt,
+      content: await mdToHtml(post.rawContent()),
+      link: `/blog/${path.basename(post.file, ".md")}`,
+    }))
+  );
+
   return rss({
     language: "ja",
     title: "Virtual Slime Blog",
     description: "ひでまるのブログサイトです",
     site: import.meta.env.SITE,
-    items: posts.map((post) => ({
-      title: post.frontmatter.title,
-      pubDate: convDateToHyphen(post.frontmatter.date),
-      description: post.frontmatter.excerpt,
-      // content: "aa",
-      link: `/blog/${path.basename(post.file, ".md")}`,
-    })),
+    items: items,
     customData: `<language>ja-jp</language>`,
   });
 };
