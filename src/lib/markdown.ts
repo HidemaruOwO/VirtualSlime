b5ea566ace8cb6824c5bc9eae97a053129d70c83
HidemaruOwO <hidemal@outlook.jp>
2023-12-01 11:21:20 +0900

    ✨ せっかくならマークダウンに含まれている画像も圧縮したくなるじゃないですか？ (src/lib/markdown.ts)

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -14,67 +14,66 @@ import * as shiki from "shiki";
 import { getImage } from "astro:assets";
 import rehypeParse from "rehype-parse";
 import { visit } from "unist-util-visit";
-import { getPlaiceholder } from "plaiceholder";
 import type { Element, Root } from "hast";
 import { importImage } from "./import";
 import { h } from "hastscript";
 
-// async function optimizeImage(html: string) {
-//   const pictureDefault = {
-//     widths: [640, 750, 828, 1080, 1200, 1920],
-//     formats: ["webp", "jpeg"],
-//   };
+async function optimizeImage(html: string) {
+  const pictureDefault = {
+    widths: [640, 750, 828, 1080, 1200, 1920],
+    formats: ["webp", "jpeg"],
+  };
 
-//   const imageNodes: {
-//     node: Element;
-//     index: number | undefined;
-//     parent: Root | Element | undefined;
-//   }[] = [];
+  const imageNodes: {
+    node: Element;
+    index: number | undefined;
+    parent: Root | Element | undefined;
+  }[] = [];
 
-//   const ast = unified().use(rehypeParse, { fragment: true }).parse(html);
+  const ast = unified().use(rehypeParse, { fragment: true }).parse(html);
 
-//   visit(ast, "element", (node, index, parent) => {
-//     if (node.tagName === "img") {
-//       imageNodes.push({ node, index, parent });
-//     }
-//   });
+  visit(ast, "element", (node, index, parent) => {
+    if (node.tagName === "img") {
+      imageNodes.push({ node, index, parent });
+    }
+  });
 
-//   const promises = imageNodes.map(async (nodeSet) => {
-//     const {
-//       node: {
-//         properties: { src, alt },
-//       },
-//       index,
-//       parent,
-//     } = nodeSet;
+  const promises = imageNodes.map(async (nodeSet) => {
+    const {
+      node: {
+        properties: { src, alt },
+      },
+      index,
+      parent,
+    } = nodeSet;
 
-//     const picture = await getImage({
-//       src: importImage(`${src}`),
-//       widths: pictureDefault.widths,
-//       formats: pictureDefault.formats,
-//     });
+    const picture = await getImage({
+      src: importImage(`${src}`),
+      widths: pictureDefault.widths,
+      formats: pictureDefault.formats,
+    });
 
-//     const imgSrc = picture.srcSet.values[0].url;
-//     // const originalResolutionSrc =
-//     // picture.srcSet.values[picture.srcSet.values.length - 1].url;
+    const imgSrc = picture.srcSet.values[0].url;
+    // const originalResolutionSrc =
+    // picture.srcSet.values[picture.srcSet.values.length - 1].url;
 
-//     const imgNode = h("img", {
-//       src: imgSrc,
-//       alt: alt,
-//       loading: "lazy",
-//       decoding: "async",
-//       width: picture.attributes.width,
-//       height: picture.attributes.height,
-//     });
-//     const figureNode = h("figure", [imgNode]);
+    const imgNode = h("img", {
+      src: imgSrc,
+      alt: alt,
+      loading: "lazy",
+      decoding: "async",
+      width: picture.attributes.width,
+      height: picture.attributes.height,
+    });
+    const figureNode = h("figure", [imgNode]);
 
-//     parent?.children.splice(index!, 1, figureNode);
-//   });
+    parent?.children.splice(index!, 1, figureNode);
+  });
 
-//   await Promise.all(promises);
+  await Promise.all(promises);
 
-//   return unified().use(rehypeStringify).stringify(ast);
-// }
+  return unified().use(rehypeStringify).stringify(ast);
+}
 
 export async function mdToHtml(md: string) {
   const highlighter = await shiki.getHighlighter({
@@ -92,6 +91,5 @@ export async function mdToHtml(md: string) {
     .use(rehypeStringify)
     .process(md);
 
-  return result.toString();
-  // return await optimizeImage(result.toString());
+  return await optimizeImage(result.toString());
 }
