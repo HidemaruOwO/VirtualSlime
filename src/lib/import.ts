1ffa3bd6e428fb3ecbe1c9a7ce351fd8fc1de237
HidemaruOwO <hidemal@outlook.jp>
2024-02-25 19:28:46 +0900

    🐛 Fix importImage function (src/lib/import.ts)

--- src/lib/import.ts
+++ src/lib/import.ts
@@ -1,6 +1,11 @@
+import fs from "fs";
+import process from "process";
+
 export function importImage(path: string) {
-  const images = import.meta.glob("/src/images/**/*.{png,jpg,jpeg,webp,gif}");
-  if (path) {
+  const images: Object = import.meta.glob(
+    "/src/images/**/*.{png,jpg,jpeg,webp,gif}"
+  );
+  if (path && fs.existsSync(`${process.cwd()}/src${path}`)) {
     return images[`/src${path}`]();
   } else {
     return images["/src/images/icons/not-found.png"]();
